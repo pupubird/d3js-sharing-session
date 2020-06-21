29bc886a3998561374348856e966e15fe701bfb5
pupubird <rainchai4240@gmail.com>
2020-06-21 15:29:05 +0800

    reformat code

--- index.js
+++ index.js
@@ -2,19 +2,34 @@
 // select all the p tag, and attach "style" attribute to the p tag with color:blue;
 d3.selectAll("p").style("color", "blue");
 
+
+
+
 // You can also select the body and do the same!
 // d3.select("body").style("background-color", "black");
+
+
+
+
 
 // the power is... you can add it dynamically!
 d3.selectAll("p").style("color", function () {
     return "hsl(" + Math.random() * 360 + ",100%,50%)";
 });
 
+
+
+
+
 // it is just a callback function!
 d3.selectAll("p").style("color", function (data, index) // params: data, index
 {
     return index % 2 ? "black" : "red";
 });
+
+
+
+
 
 // now here comes the super power... adding the data!
 d3.selectAll("p")
@@ -26,6 +41,10 @@ d3.selectAll("p")
         console.log("I am a callback function called on every data " + d)
         return d + "px";
     });
+
+
+
+
 
 // weird behaviour? only shown the first two data? you need an enter function!
 let p = d3.selectAll("p")
@@ -39,6 +58,10 @@ p.data([4, 8, 15, 16, 23, 42])
         console.log("I am a callback function called on every data " + d)
         return d + "px";
     });
+
+
+
+
 
 // to update
 p.data([102, 112, 122, 132, 142])
